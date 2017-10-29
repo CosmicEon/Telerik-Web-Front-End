bc47c32d6c1fbd004edcf9d38fc1c146e9b489a2
CosmicEon <sillimanite90@gmail.com>
2017-10-29 17:52:29 +0200

    port correction

--- api/config/constants.js
+++ api/config/constants.js
@@ -1,6 +1,6 @@
 const constants = {
     mongoDbConnection: 'mongodb://testuser2:test12345@ds237445.mlab.com:37445/web-course-project', // 'mongodb://localhost:27017/web-course-project'
-    serverPort: 8080,
+    serverPort: process.env.PORT || 8080,
 };
 
 module.exports = { constants };
